40d804bcbc8cd1dccffbd1612b0a8275885d4861
Edson <netosilvan@hotmail.com>
2016-06-11 17:46:48 +0300

    adding all the files

---
+++ BugHackers_wbsite/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bugHackersApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bugHackersApp
+ */
+angular.module('bugHackersApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ BugHackers_wbsite/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bugHackersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bugHackersApp
+ */
+angular.module('bugHackersApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
